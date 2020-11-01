46e361a2e7dfabd882091334189ae753b4256ee6
Nicholas Pecka <npecka@iastate.edu>
2020-11-01 01:02:47 -0600

    Revised RMS functions to make them accept multiple task sets. Added lcm function for rms. Flushed out priority order function with proper handling of multiple task sets.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 import numpy
+from math import gcd
 
 # Input is task set
 # Run utilization test
@@ -13,46 +14,77 @@ import numpy
 class RMS:
 
     # Expected task format ci, pi, dl
-    def __init__(self, task1, task2):
-        self.task1 = task1
-        self.task2 = task2
+    def __init__(self, task):
+        self.task = task
 
     # Utilization test will take ci/pi of each task
     # It will then add them all together
     # Then check if it is < n(2^1/n - 1)
     # Where n is # of tasks
     def utilization_test(self):
-        util1 = float(self.task1[0]) / float(self.task1[1])
-        util2 = float(self.task2[0]) / float(self.task2[1])
-        util3 = util1 + util2
-        # u_test = n(2^1/n - 1)
-        if util3 <= 0.82:
+        u_task = 0
+        for x in range(len(task_sets)):
+            u_task = u_task + (float(self.task[x][0]) / float(self.task[x][1]))
+        u_test = len(self.task) * (pow(2, 1 / len(self.task)) - 1)
+        if u_task <= u_test:
             return True
         else:
             return False
 
     def exact_analysis(self):
-        self.speed += 5
+        flag = self.priority_order()
 
     # compare task priorities
     # lowest priority wins
+    # create array of tasks in order
+    # highest to lowest priority order
     def priority_order(self):
-        if self.task1 <= self.task2:
-            return True
-        else:
-            return False
+        ordered_array = []
+        for x in range(len(self.task)):
+            copy_array = self.task
+            temp = [copy_array[0]]
+            for y in range(len(copy_array)):
+                if temp[0][1] >= copy_array[y][1]:
+                    temp = [copy_array[y]]
+            ordered_array.append(temp[0])
+            copy_array.remove(temp[0])
+        return ordered_array
+
+    def lcm_rms(self):
+        list_of_deadlines = []
+        for x in range(len(self.task)):
+            list_of_deadlines.append(int(self.task[x][2]))
+        lcm = list_of_deadlines[0]
+        for i in list_of_deadlines[1:]:
+            lcm = lcm * i // gcd(lcm, i)
+        return lcm
 
     # schedule task based on lowest priority
     # schedule up to LCM of the deadlines
     # Phase 1: print array of tasks in order
     # Phase final: prints excel style on GUI
     def rms_schedule(self):
-        flag = self.priority_order()
-        lcm = numpy.lcm(int(self.task1[1]), int(self.task2[1]))
+        p_order = self.priority_order()
+        lcm = self.lcm_rms()
+
+        # TODO
+
+        flag = True
+        array = []
         if flag is True:
-            firstTask = lcm / self.task1[2]
+            first_task = numpy.floor(lcm / int(self.task1[2]))
+            second_task = numpy.floor(lcm / int(self.task2[2]))
+            amount_to_be_scheduled = int(first_task) + int(second_task)
+            for x in range(amount_to_be_scheduled):
+                if flag is True:
+                    array.append("T1")
+                    flag = False
+                else:
+                    array.append("T2")
+                    flag = True
+            print(array)
         else:
-            firstTask = lcm / self.task2[2]
+            first_task = numpy.floor(lcm / int(self.task2[2]))
 
 
 # Input is task set
@@ -67,9 +99,9 @@ class RMS:
 
 class EDF:
 
-    def __init__(self, task1, task2):
-        self.task1 = task1
-        self.task2 = task2
+    def __init__(self, task):
+        self.task = task
+        print(task)
 
     # Utilization test will take ci/pi of each task
     # It will then add them all together
@@ -77,14 +109,14 @@ class EDF:
     # Where n is # of tasks
     # Necessary for EDF and sufficient
     def utilization_test(self):
-        util1 = float(self.task1[0]) / float(self.task1[1])
-        util2 = float(self.task2[0]) / float(self.task2[1])
-        util3 = util1 + util2
+        util1 = float(self.task[0]) / float(self.task[1])
+        #util2 = float(self.task2[0]) / float(self.task2[1])
+        #util3 = util1 + util2
         # u_test = n(2^1/n - 1)
-        if util3 <= 1:
-            return True
-        else:
-            return False
+        #if util3 <= 1:
+        #    return True
+        #else:
+        #    return False
 
 # Two entries, RMS and EDF
 # Input is a task set
@@ -96,23 +128,23 @@ if __name__ == '__main__':
 
     while True:
 
-        action = input("Please input 2 task sets in format \"Ci, Pi, Dl\", \"Ci, Pi, Dl\" ")
-        if len(action) == 6:
-            print(action)
+        action = input("Please input at least 2 task sets in format Ci Pi Dl, Ci Pi Dl ")
+        replace = action.replace(" ", "")
+        task_sets = replace.split(",")
+
+        if len(task_sets) >= 2:
+            print(task_sets)
         else:
             print("Invalid task set entered")
             break
-        t1 = action[0:3]
-        t2 = action[3:6]
 
-        print(t1)
-        print(t2)
+        rms = RMS(task_sets)
+        #edf = EDF(task_sets)
 
-        rms = RMS(t1, t2)
-        edf = EDF(t1, t2)
-
-        print(rms.utilization_test())
-        print(rms.rms_schedule())
+        #print(rms.utilization_test())
+        #rms.priority_order()
+        #rms.lcm_rms()
+        #print(rms.rms_schedule())
 
         # TKinter setup for GUI of scheduling
 
