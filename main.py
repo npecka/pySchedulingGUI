f4b32922fec2a33ad718932ced80f879a86ea740
zangoose107 <zangoose107@gmail.com>
2020-11-15 02:17:20 -0600

    Added excel file input and output for task sets. Now users can feed an excel sheet with multiple task sets and the program will iterate through the excel file and arrange the schedules in the proper format (currently for RMS soon both) and print to a new excel sheet with proper arrangement. Also added new buttons to GUI and resized for proper viewing. Users can preview an inputted task set manually and then point to excel file for mass input.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import numpy as np
+import openpyxl
+from openpyxl.styles import Alignment
 from rms import RMS
 from edf import EDF
 
@@ -34,24 +36,28 @@ if __name__ == '__main__':
 
             if len(tasks) < 6:
                 print("Invalid task set entered")
-                # break
+                label = tk.Label(root, text="The task set can not be scheduled")
+                label.pack()
             else:
                 return task_sets
 
             #choice_case = choice.lower()
 
 
-        def rms_scheduler():
-            string = e.get()
-            task_sets = scheduling_setup(string)
-            #if choice_case == "rms" or choice_case == "both":
+        def rms_scheduler(string):
+            if string == '':
+                string_input = e.get()
+                task_sets = scheduling_setup(string_input)
+            else:
+                task_sets = scheduling_setup(string)
             print("This is rms")
             rms = RMS(task_sets)
             least_common_multiple = rms.lcm_rms(task_sets)
             priority_order = rms.priority_order(task_sets)
             flag = rms.exact_analysis()
             if flag is False:
-                print("Unschedulable")
+                label = tk.Label(root, text="The task set can not be scheduled for RMS")
+                label.pack()
             else:
                 rms_schedule = rms.rms_schedule(priority_order, least_common_multiple, task_sets)
                 element = ''
@@ -61,18 +67,22 @@ if __name__ == '__main__':
                 label2 = tk.Label(root, text=element)
                 label.pack()
                 label2.pack()
+                return rms_schedule
 
-        def edf_scheduler():
-            string = e.get()
-            task_sets = scheduling_setup(string)
-            #if choice_case == "edf" or choice_case == "both":
+        def edf_scheduler(string):
+            if string == '':
+                string_input = e.get()
+                task_sets = scheduling_setup(string_input)
+            else:
+                task_sets = scheduling_setup(string)
             print("This is edf")
             edf = EDF(task_sets)
             l_c_m = edf.lcm_edf(task_sets)
             p_ord = edf.priority_order(task_sets)[0]
             flag = edf.utilization_test(task_sets)
             if flag is False:
-                print("Unschedulable")
+                label = tk.Label(root, text="The task set can not be scheduled for EDF")
+                label.pack()
             else:
                 edf_schedule = edf.edf_schedule(p_ord, l_c_m, task_sets)
                 element = ''
@@ -82,19 +92,118 @@ if __name__ == '__main__':
                 label2 = tk.Label(root, text=element)
                 label.pack()
                 label2.pack()
+                return edf_schedule
+
+        def print_scheduler_rms(string, iteration, number_of_tasks):
+            wb = openpyxl.load_workbook('updatedResults.xlsx')
+            sheet = wb['results']
+            arr = rms_scheduler(string)
+
+            for i in range(len(arr)):
+                for j in range(number_of_tasks):
+                    string_check = 'T' + str(j + 1)
+                    if arr[i] == ' ':
+                        my_cell = sheet.cell(row=(number_of_tasks + 1 + iteration), column=(i + 1))
+                        my_cell.value = i
+                        my_cell.alignment = Alignment(horizontal='right')
+                        break
+                    if arr[i] == string_check:
+                        my_cell = sheet.cell(row=(j + 1 + iteration), column=(i + 2))
+                        my_cell.value = arr[i]
+                        my_cell.alignment = Alignment(horizontal='center')
+                        my_cell = sheet.cell(row=(number_of_tasks + 1 + iteration), column=(i+1))
+                        my_cell.value = i
+                        my_cell.alignment = Alignment(horizontal='right')
+            my_cell = sheet.cell(row=(number_of_tasks + 1 + iteration), column=(len(arr) + 1))
+            my_cell.value = len(arr)
+            my_cell.alignment = Alignment(horizontal='right')
+            wb.save('updatedResults.xlsx')
+            point_in_sheet = number_of_tasks + 2
+            return point_in_sheet
+
+
+        def print_scheduler_edf(string):
+            wb = openpyxl.load_workbook('results.xlsx')
+            sheet = wb['results']
+            arr = edf_scheduler(string)
+            number_of_tasks = int(len(arr) / 3)
+
+            for i in range(len(arr)):
+                for j in range(number_of_tasks):
+                    string_check = 'T' + str(j + 1)
+                    if arr[i] == string_check:
+                        my_cell = sheet.cell(row=(j + 1), column=(i + 2))
+                        my_cell.value = arr[i]
+                        my_cell.alignment = Alignment(horizontal='center')
+                        my_cell = sheet.cell(row=(number_of_tasks + 1), column=(i + 1))
+                        my_cell.value = i
+                        my_cell.alignment = Alignment(horizontal='right')
+            my_cell = sheet.cell(row=(number_of_tasks + 1), column=(len(arr) + 1))
+            my_cell.value = len(arr)
+            my_cell.alignment = Alignment(horizontal='right')
+            wb.save('updatedResults.xlsx')
+
+        def file_schedule_setup():
+            string = e.get()
+            wb = openpyxl.load_workbook(string)
+            sheet = wb['results']
+            rows = sheet.max_row
+            cols = sheet.max_column
+            iterator = 0
+            for i in range(rows):
+                task_string = ""
+                task_count = 0
+                for j in range(cols):
+                    if sheet.cell(row=i+1, column=j+1).value is None:
+                        break
+                    my_cell = sheet.cell(row=i + 1, column=j + 1)
+                    task_string = task_string + my_cell.value
+                    if sheet.cell(row=i + 1, column=j + 2).value is not None:
+                        task_string = task_string + ', '
+                    task_count += 1
+                iterator += print_scheduler_rms(task_string, iterator, task_count)
+            # wb.save('updatedResults.xlsx')
+
 
         root = tk.Tk()
-        root.title("RMS/EDF Scheduling\nPlease specify Ci Pi Di")
+        root.title("RMS/EDF Scheduling           Please specify Ci Pi Di, Ci Pi Di")
+        root.geometry("500x300")
 
         e = tk.Entry(root)
-        e.pack()
+        e.place(relx = 0.5,
+                rely = 0.5,
+                anchor = "center")
         e.focus_set()
 
-        #b.pack(side='bottom')
-
-        button = tk.Button(root, text="RMS", command=rms_scheduler)
-        button2 = tk.Button(root, text="EDF", command=edf_scheduler)
-        button.pack()
-        button2.pack()
+        button = tk.Button(root, text="RMS\nPreview", command=rms_scheduler)
+        button.config(width=10)
+        button.config(height=3)
+        button.place(relx = 0.0,
+                     rely = 1.0,
+                     anchor = 'sw')
+        button2 = tk.Button(root, text="EDF\nPreview", command=edf_scheduler)
+        button2.config(width=10)
+        button2.config(height=3)
+        button2.place(relx=1.0,
+                     rely=1.0,
+                     anchor='se')
+        button3 = tk.Button(root, text="RMS to Excel", command=print_scheduler_rms)
+        button3.config(width=10)
+        button3.config(height=3)
+        button3.place(relx=0.3,
+                      rely=1.0,
+                      anchor='s')
+        button4 = tk.Button(root, text="EDF to Excel", command=print_scheduler_edf)
+        button4.config(width=10)
+        button4.config(height=3)
+        button4.place(relx=0.7,
+                      rely=1.0,
+                      anchor='s')
+        button5 = tk.Button(root, text="Convert list\nfrom file", command=file_schedule_setup)
+        button5.config(width=10)
+        button5.config(height=3)
+        button5.place(relx=0.5,
+                      rely=1.0,
+                      anchor='s')
 
         root.mainloop()
